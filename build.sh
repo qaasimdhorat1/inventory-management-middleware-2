111a628edd56c84450aef8d137f4c0da920a57d7
qaasimdhorat1 <qaasim.dhorat@ada.ac.uk>
2026-03-28 16:59:06 +0000

    fix: correct build.sh and Procfile for Render deployment

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-```
-
-Then create a file called `Procfile` (no extension, capital P) in the root:
-```
-web: gunicorn config.wsgi:application
